77778d36752726012ca97a70db971829940259d2
LITEBYTE <litebyteco@gmail.com>
2026-01-31 23:57:04 +0100

    Update router.go

--- router/router.go
+++ router/router.go
@@ -102,6 +102,8 @@ func Configure(m *wserver.Manager, client remote.Client) *gin.Engine {
 			files.POST("/compress", postServerCompressFiles)
 			files.POST("/decompress", postServerDecompressFiles)
 			files.POST("/chmod", postServerChmodFile)
+			files.POST("/size", getFolderSize)
+
 
 			files.GET("/pull", middleware.RemoteDownloadEnabled(), getServerPullingFiles)
 			files.POST("/pull", middleware.RemoteDownloadEnabled(), postServerPullRemoteFile)
